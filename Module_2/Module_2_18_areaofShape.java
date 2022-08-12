36ba45966890d068aee21f5de7e0bc3969a5634e
Ajay Gohil <91679702+Ajaygohil25@users.noreply.github.com>
2022-08-12 09:54:16 +0530

    Update Module_2_18_areaofShape.java

--- Module_2/Module_2_18_areaofShape.java
+++ Module_2/Module_2_18_areaofShape.java
@@ -1,5 +1,52 @@
 package Module_2;
 
-public class Module_2_18_areaofShape {
+// area of rectangle --> A=wl
+// area of square --> A=a^2
+// area of circle --> A = pi r^2
+abstract class Shape
+{
+	abstract public void RectangleArea(float w, float l);
+	abstract public void SquareArea(float a);
+	abstract public void CircleArea(float r);	
+}
+class Area extends Shape
+{
 
+	@Override
+	public void RectangleArea(float w, float l) 
+	{
+		float area;
+		 area  = w*l;
+		 System.out.println("Area of Recantangle is : "+area);
+		
+	}
+
+	@Override
+	public void SquareArea(float a)
+	{
+		float area;
+		area = a*a;
+		System.out.println("Area of Square is : "+area);
+		
+	}
+
+	@Override
+	public void CircleArea(float r) 
+	{
+		float area;
+		area = (float) ((r*r)* 3.14);
+		System.out.println("Area of Circle is : "+area);
+	}
+	
+}
+public class Module_2_18_areaofShape
+{
+	public static void main(String[] args)
+	{
+		Area a = new Area();
+		a.RectangleArea(2f, 3f);
+		a.SquareArea(4f);
+		a.CircleArea(5f);
+		
+	}
 }
